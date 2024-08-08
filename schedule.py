c4ea3e3bc9093170809494a929e2586b7a0591d3
ebatelchyrok <134564163+ebatelchyrok@users.noreply.github.com>
2024-08-09 01:38:39 +0300

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -2,4 +2,4 @@ from plugins.scheduled import infect
 from app import scheduler
 
 def jobs ():
-    scheduler.add_job(infect, 'cron', hour=0, minute=0)
+    scheduler.add_job(infect, 'cron', hour=0, minute=0, misfire_grace_time=3600)
