95e9efb1ade32f02dba2fe8777667dd686d3aec2
ebatelchyrok <samariums@icloud.com>
2024-08-08 19:51:46 +0300

    fix

--- plugins/handlers/save_ttl.py
+++ plugins/handlers/save_ttl.py
@@ -8,12 +8,12 @@ async def save_ttl(app: Client, message: Message):
     chat = await app.get_chat(message.chat.id)
     match message.media:
         case MessageMediaType.PHOTO:
-            if not message.photo.ttl_seconds and message.photo.ttl_seconds >= 60:
+            if not message.photo.ttl_seconds:
                 return
             file = await message.download(in_memory=True)
             await app.send_photo ("me", file, caption=f"Получено из {chat.title}", has_spoiler=True)
         case MessageMediaType.VIDEO:
-            if not message.video.ttl_seconds and message.photo.ttl_seconds >= 60:
+            if not message.video.ttl_seconds:
                 return
             file = await message.download(in_memory=True)
             await app.send_video ("me", file, caption=f"Получено из {chat.title}", has_spoiler=True)
