d586d82c7e530e4b528f6959b903b91700647838
ebatelchyrok <samariums@icloud.com>
2024-08-10 01:01:09 +0300

    fix

--- plugins/scheduled/iris_virus.py
+++ plugins/scheduled/iris_virus.py
@@ -4,9 +4,9 @@ from asyncio import sleep
 
 
 async def infect():
-    current_time = datetime.now(tz=tz)
-    for i in range(1, 73):
-        schedule_time = current_time + timedelta(minutes=20 * i)
-        await app.send_message(chat_id=int (-1002175276391), text="Заразить +", schedule_date=schedule_time)
+    schudled_time = datetime.now(tz=tz)
+    for _ in range(72):
+        schudled_time += timedelta(minutes=20)
+        await app.send_message(chat_id=int (-1002175276391), text="Заразить +", schedule_date=schudled_time)
         await sleep(7)
         
