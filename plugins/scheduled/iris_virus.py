3a19507b1ed6bbb1b071421812c24c7a62d0a8d3
ebatelchyrok <134564163+ebatelchyrok@users.noreply.github.com>
2024-08-13 06:05:07 +0300

    Update iris_virus.py

--- plugins/scheduled/iris_virus.py
+++ plugins/scheduled/iris_virus.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from asyncio import sleep
 
 async def infect():
-    for i in range(72):
+    for i in range(1, 73):
         schudled_time = datetime.now(tz=tz) + timedelta(minutes=20 * i)
         await app.send_message(chat_id=int(-1002175276391), text="Заразить +", schedule_date=schudled_time)
         await sleep(3)
